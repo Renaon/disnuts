f9feb194cc5dc4ceb263460e608f0dc8405e54c1
Renaon <egorbaturin2013@yandex.ru>
2021-02-16 12:11:40 +0300

    Autism with 1C

--- controller/GetBonuce.php
+++ controller/GetBonuce.php
@@ -2,16 +2,19 @@
 
     class GetBonuce extends Docking
     {
-
+        private $result;
         public function __construct($client)
         {
-            // $_SERVER['REQUEST_TIME']
-//            $time["ДатаЗапроса"] = date('Y-m-d H:i:s');
-//            $id["ИдентификаторКарты"] = '04';
             $params = array(
                 'ИдентификаторКарты' => '739f3250-53d7-11eb-a2cb-704d7b28f39a',
                 'ДатаЗапроса' => date('Y-m-d H:i:s')
             );
             $result = $client->ПолучитьОстатокБонусов($params);
+            var_dump($result);
+            $this->result = $result;
+        }
+
+        public function getResult(){
+            return $this->result->{'return'}->{'РезультатЗапроса'}->{'КоличествоБаллов'};
         }
     }
